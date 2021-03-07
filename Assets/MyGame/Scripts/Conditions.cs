184e456e9b0160991527031f0c9467849db77250
pklacher <pia.lacher04@htl-salzburg.ac.at>
2021-03-08 00:03:34 +0100

    almost finished, button does not work

--- Assets/MyGame/Scripts/Conditions.cs
+++ Assets/MyGame/Scripts/Conditions.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Conditions : MonoBehaviour
 {
+    private bool variableA = false;
+    //four numbers greater than five
+    private bool variableB = false;
+    //Spacebar pressed for 3sec
+    private bool variableC = false;
+    //numbers 1-4 pressed
+    private bool variableD = false;
+    //
+
+    private bool numberOne = false;
+    private bool numberTwo = false;
+    private bool numberThree = false;
+    private bool numberFour = false;
+
+
+    private float time;
 
     private int CTry;
 
@@ -16,96 +32,86 @@ public class Conditions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        NumberCTry();
-       /* if (VariableA && VariableB == true)
+        if ((variableA && variableB || variableB && variableC || variableC && variableD))
         {
-            Debug.Log("A && B is true");
+            Debug.Log("conditions completed");
+        }
+        else
+        {
+            //Debug.Log("conditions not completed");
         }
 
-        if (VariableB && VariableC == true)
+        if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("B && C is true");
+            time += Time.deltaTime;
         }
 
-        if (VariableC && VariableD == true)
+        if (Input.GetKeyUp(KeyCode.Space)){
+            time = 0;
+        }
+
+        if (time >= 3)
         {
-            Debug.Log("C && D is true");
-        }*/
-    }
-
-    private bool VariableA()
-    {
-        return false;
-    }
-
-    private bool VariableB()
-    {
-        return false;
-    }
-
-    private bool NumberC()
-    {
-        
-         if (Input.GetKeyDown(KeyCode.Alpha1))
-         {
-            Debug.Log("1 pressed");
-             if (Input.GetKeyDown(KeyCode.Alpha2))
-             {
-                Debug.Log("2 pressed");
-                 if (Input.GetKeyDown(KeyCode.Alpha3))
-                 {
-                    Debug.Log("3 pressed");
-                    if (Input.GetKeyDown(KeyCode.Alpha4))
-                     {
-                         Debug.Log("Variable C is right");
-                         return true;
-                     }
-                 }
-                 return false;
-             }
-             return false;
-         }
-         return false;
-    }
-
-    private void NumberCTry()
-    {
-        CTry = 0;
-        Input.GetKeyDown(KeyCode.UpArrow)
+            variableB = true;
+            //Debug.Log("Variable B true");
+        }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            CTry = 1;
-            Debug.Log("1 pressed");
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                CTry = 2;
-                Debug.Log("2 pressed");
-                if (Input.GetKeyDown(KeyCode.Alpha3))
-                {
-                    CTry = 3;
-                    Debug.Log("3 pressed");
-                    if (Input.GetKeyDown(KeyCode.Alpha4))
-                    {
-                        CTry = 4;
-                        Debug.Log("Variable C is right");
-
-                    }
-                }
-
-            }
-
+            numberOne = true;
         }
-       
-        
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            numberTwo = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            numberThree = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            numberFour = true;
+        }
+
+        if (numberOne && numberTwo && numberThree && numberFour)
+        {
+            variableC = true;
+            //Debug.Log("Variable C true");
+        }
+
+        RandomNumbers();
+        ButtonPressed();
     }
 
- 
-
-    private bool VariableD()
+    public void ButtonPressed()
     {
-        return false;
+        variableD = true;
+        Debug.Log("Variable D true");
     }
+
+    public void RandomNumbers()
+    {
+        int greaterFive = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int rnd = Random.RandomRange(1, 11);
+            if (rnd > 5)
+            {
+                greaterFive++;
+            }
+        }
+        if (greaterFive >= 4)
+        {
+            variableA = true;
+            //Debug.Log("Variable A true");
+        }
+    }
+
+
 }
 
 
---
+++ Assets/MyGame/Scripts/ConditionsTwo.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionsTwo : MonoBehaviour
+{
+    private bool SpacePressed = false;
+    private float time;
+
+    private bool spacePressed;
+    private bool buttonPressed;
+    private bool fourGreaterThanFive;
+    private bool numbersPressed;
+
+    private bool keyOne;
+    private bool keyTwo;
+    private bool keyThree;
+    private bool keyFour;
+
+    public void Update()
+    {
+        if ((spacePressed && buttonPressed) || (buttonPressed && fourGreaterThanFive) || (fourGreaterThanFive && numbersPressed))
+        {
+            Debug.Log("true");
+        }
+        else
+        {
+            Debug.Log("false");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            time += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            time = 0;
+        }
+        if (time > 3)
+        {
+            spacePressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            keyOne = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            keyTwo = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            keyThree = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            keyFour = true;
+        }
+
+        if (keyOne && keyTwo && keyThree && keyFour)
+        {
+            numbersPressed = true;
+        }
+    }
+
+    public void ButtonPressed()
+    {
+        buttonPressed = true;
+    }
+
+    public void GenerateNumbers()
+    {
+        int numGreaterThanFive = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int number = Random.Range(1, 11);
+            if (number > 5)
+            {
+                numGreaterThanFive++;
+            }
+        }
+
+        if (numGreaterThanFive >= 4)
+        {
+            fourGreaterThanFive = true;
+        }
+    }
+}
